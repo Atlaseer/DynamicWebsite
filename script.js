c072efb490ea53c4fd45a1342e0ce591de6a8e98
Simon Persson <atlaspersson@outlook.com>
2025-03-12 17:23:08 +0100

    Dynamic API now displayable on HTML

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const postContainer = document.getElementById("post-container");
 const userModal = document.getElementById("user-modal");
 const userName = document.getElementById("user-name");
-const userMail = document.getElementById("user-mail");
-const userAddress = document.getElementById("user.address");
+const userMail = document.getElementById("user-email");
+const userAddress = document.getElementById("user-address");
 const closeModal = document.querySelector(".close");
 
 const API_POSTS = "https://dummyjson.com/posts";
@@ -27,13 +27,13 @@ async function fetchPosts(){
     const data = await res.json();
 
     for (let post of data.posts){
-        await displayPost(post);
+        await displayPosts(post);
     }
 }
 
 //Fetches and returns the comments based on post ID
 async function fetchComments(postId){
-    const res = await fetch('${API_COMMENTS}/post/${postId}')
+    const res = await fetch(`${API_COMMENTS}/post/${postId}`)
     const data = await res.json();
     return data.comments
 }
@@ -43,7 +43,7 @@ async function displayPosts(post) {
     postElement.classList.add("post");   
 
     const comments = await fetchComments(post.id);
-    const commentsHTML = comments.map(comment=> `<p>💬 ${comment.body}</p>`).join("")
+    const commentsHTML = comments.map(comment=> `<p> ${comment.body}</p>`).join("")
 
     const user = usersData[post.userId] || { username: "Unknown" };
 
@@ -51,7 +51,7 @@ async function displayPosts(post) {
     <h3>${post.title}</h3>
     <p>${post.body}</p> 
     <p>Tags: ${post.tags.join(", ")}</p>    
-    <p>Reactions: ❤️ ${post.reactions}</p>
+    <p>Reactions:  ${post.reactions}</p>
     <p>By: <span class="user-link" data-userid="${post.userId}">${user.username}</span></p>
     <div class="comments">
     <h4>Comments:</h4>
